dcd23efc748c200acfa9c38cb082a4677b6f6476
Eduardo Hellas <Eduardo.Hellas@sas.com>
2020-06-19 19:31:29 -0300

    model validation fix

--- model_validation.py
+++ model_validation.py
@@ -4,6 +4,14 @@ Created on Fri Jun 19 19:25:33 2020
 
 @author: edhell
 """
+import swat
+conn = swat.CAS(#'pdcesx12091.exnet.sas.com', port=8777, protocol = 'http',
+            'localhost', port = 5570, ## bug on swat 1.6.0
+            #caslib = 'public', 
+            username = 'sasdemo',
+            password = 'Orion123')#, session = session_id)
+
+conn.loadactionset("percentile")
 
 ################################################
 ######### FIT STATISTICS #######################
